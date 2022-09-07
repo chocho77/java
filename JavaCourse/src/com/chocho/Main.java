0e7e6229774221e90b33c7f36477f8bf0aa74838
chocho77 <chavmom@gmail.com>
2022-09-07 22:45:58 +0300

    Update Main.java

--- JavaCourse/src/com/chocho/Main.java
+++ JavaCourse/src/com/chocho/Main.java
@@ -1,8 +1,14 @@
 package com.chocho;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(9);
         
         System.out.println("Hooray my first java app");
     }
