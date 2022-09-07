651db8415c047b9764c00000db318fb8ec1e6967
chocho77 <chavmom@gmail.com>
2022-09-07 23:47:22 +0300

    Update Main.java

--- vcode/com/company/main/Main.java
+++ vcode/com/company/main/Main.java
@@ -1,7 +1,12 @@
 package com.company.main;
 
+import com.company.resources.MyClassImpl;
+
 public class Main {
     public static void main(String[] args) {
+        MyClassImpl myClassImpl = new MyClassImpl();
+        int res = myClassImpl.sub(2, 3);
+        System.out.println("res = " + res);
         System.out.println("Test message.");
         System.out.println("Test message.");
     }
