7ec79fac7fcc4f8a5e91692e0ac60d814eb0445f
chocho77 <chavmom@gmail.com>
2022-09-07 23:45:53 +0300

    develop program

--- vcode/com/company/resources/MyClassImpl.java
+++ vcode/com/company/resources/MyClassImpl.java
@@ -2,4 +2,24 @@ package com.company.resources;
 
 public class MyClassImpl {
 
+    private int a;
+
+    private int b;
+
+    public MyClassImpl() {
+    }
+
+    public MyClassImpl(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sub(int a, int b) {
+
+        this.a = a;
+        this.b = b;
+
+        return a + b;
+    }
+
 }
