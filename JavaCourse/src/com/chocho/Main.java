00cb9aa6f87c3932fad48dd7fdaec0148a5a962f
chocho77 <chavmom@gmail.com>
2022-09-07 22:49:37 +0300

    Update Main.java

--- JavaCourse/src/com/chocho/Main.java
+++ JavaCourse/src/com/chocho/Main.java
@@ -9,6 +9,7 @@ public class Main {
         List<Integer> list = new ArrayList<>();
         list.add(5);
         list.add(9);
+        list.forEach(integer -> list.get(0));
         
         System.out.println("Hooray my first java app");
     }
