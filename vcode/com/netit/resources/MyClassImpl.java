7580c0b05c9e8d5ab25b526a4853cd8468c95f0e
chocho77 <chavmom@gmail.com>
2022-09-08 01:04:57 +0300

    create new class

---
+++ vcode/com/netit/resources/MyClassImpl.java
@@ -0,0 +1,39 @@
+package com.netit.resources;
+
+public class MyClassImpl {
+
+    private double a = 0;
+    private double b = 0;
+    private double c = 0;
+
+    public MyClassImpl(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+}
