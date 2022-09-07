7a28d2fc77c7315fe38aa98995fe1cac6af9fe91
chocho77 <chavmom@gmail.com>
2022-09-08 01:10:35 +0300

    edit main class

--- vcode/com/netit/main/Main.java
+++ vcode/com/netit/main/Main.java
@@ -1,8 +1,16 @@
 package com.netit.main;
 
+import com.netit.resources.MyClassImpl;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        MyClassImpl myClassImpl = new MyClassImpl(2.1, 3.4, 5.6);
+
+        double res = myClassImpl.getA();
+
+        System.out.println("res = " + res);
 
     }
 
